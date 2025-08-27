e7d388a37c39408d3e6091a869032028bdd2fcd1
Carolina <krol_olive@hotmail.com>
2025-08-27 20:51:53 -0300

    first commit

--- src/biblioteca.test.js
+++ src/biblioteca.test.js
@@ -10,7 +10,7 @@ expect(resultado).toBe("Erro: Nome é obrigatório!")
 
 test('verificar se o autor do livro é obrigatorio', function() {
 
-const resultado = bibliotecaService.criarLivro("nome", '', 2015, 'ficcao')
+const resultado = bibliotecaService.criarLivro("nome", 'Kaique', 2015, 'ficcao')
 
 expect(resultado).toBe("Erro: Autor é obrigatório!")
 
@@ -24,8 +24,37 @@ test('deve retornar erro se o ano de publicação não for informado', function 
 
 
 test('verificar se o ano de publicação está vazio', function() {
-  const resultado = bibliotecaService.criarLivro("nome", "Kaique", "", "ficcao");
+  const resultado = bibliotecaService.criarLivro("nome", "Kaique", "2015", "ficcao");
 
   expect(resultado).toBe("Erro: Ano de publicação é obrigatório!");
   
 });
+
+
+test('deve retornar erro se o título não for informado', function() {
+  const resultado = bibliotecaService.buscarLivroPorNome("nome", "Kaique", "2015", "");
+
+  expect(resultado).toBe("Erro: Título é obrigatório para busca");
+});
+
+  
+test('deve retornar erro se o título não for informado na busca', function () {
+  const resultado = bibliotecaService.buscarLivroPorNome("", "Kaique", "2015", "ficcao");
+
+  expect(resultado).toBe("Erro: Título é obrigatório para busca");
+});
+
+test('deve retornar erro se o título estiver vazio na busca', function () {
+  const resultado = bibliotecaService.buscarLivroPorNome("", "Kaique", "2015", "");
+
+  expect(resultado).toBe("Erro: Título é obrigatório para busca");
+});
+
+
+test('verificar se a busca por categoria retorna com sucesso', function () {
+
+  const resultado = bibliotecaService.buscarLivroPorCategoria("");
+
+  expect(resultado).toBe("Erro: Categoria é obrigatória para busca");
+});
+
